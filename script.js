77e0c7f4955cd9220e04c93510c2e23daafd9d77
Donnahue George <Donnahuegjr@gmail.com>
2020-12-15 11:27:07 -0500

    getting the AJAX call ready and redid somoe ids

---
+++ script.js
@@ -0,0 +1,17 @@
+// variables that will be needed
+
+
+//API information
+
+//AJAX call
+
+
+//submit button
+$("#submitBtn").on("click", function(event) {
+    event.preventDefault()
+   
+
+    var city = $("#citySearch").val()
+    console.log(city)
+})
+// functions to link to page
