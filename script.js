240ba6330219451fd01fb94e0b4408dad7f4b284
Donnahue George <Donnahuegjr@gmail.com>
2020-12-15 13:27:04 -0500

    got ajax to work properly

--- script.js
+++ script.js
@@ -3,15 +3,22 @@
 
 //API information
 
-//AJAX call
 
 
-//submit button
+//submit button & AJAX CALL
 $("#submitBtn").on("click", function(event) {
     event.preventDefault()
    
-
     var city = $("#citySearch").val()
     console.log(city)
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=348a03864c53988b715b0daea933a0ef";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+      });
+
 })
 // functions to link to page
